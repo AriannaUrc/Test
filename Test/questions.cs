5304808c6ddebf1b792467c3384c41979ad72224
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2024-01-11 10:52:12 +0100

    Aggiungere i file di progetto.

---
+++ Test/questions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal abstract class questions
+    {
+        public abstract string Descrizione();
+        public abstract int Punteggio();
+    }
+}
