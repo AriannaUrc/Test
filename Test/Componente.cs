e586fb6cd782b235ed94c0d6260decef8a335476
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2024-01-23 12:41:20 +0100

    classe domande e componente

---
+++ Test/Componente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal abstract class Componente
+    {
+        public abstract void Add(object obj);
+        public abstract object GetChild(int index);
+        public abstract void Remove(int index);
+        public abstract string ToString(object obj);
+        public abstract override bool Equals(object obj);
+        public abstract override int GetHashCode();
+        public abstract int Punteggio();
+    }
+}
---
+++ Test/Domanda.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Domanda : Componente
+    {
+        List<Componente> risposte;
+        public override void Add(object obj)
+        {
+            risposte.Add((obj as Componente));
+        }
+        public override object GetChild(int index)
+        {
+            return risposte[index];
+        }
+        public override void Remove(int index)
+        {
+            risposte.RemoveAt(index);
+        }
+        public override string ToString(object obj)
+        {
+            return risposte.ToString();
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return (ToString() == obj.ToString());
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return risposte.GetHashCode();
+        }
+        public override int Punteggio()
+        {
+            return 0;
+        }
+    }
+}
